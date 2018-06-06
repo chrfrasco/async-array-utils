2317e33b26e31b109fd2d7803ce05836a1e6373c
Christian Scott <christianfscott@gmail.com>
2018-06-06 19:19:18 +1200

    ensure that forEach runs asynchronously

--- src/forEach.test.ts
+++ src/forEach.test.ts
@@ -11,3 +11,18 @@ test("provides callback the same arguments as Array.prototype.forEach", async ()
 
   expect(cbAsync.mock.calls).toEqual(cbSync.mock.calls);
 });
+
+test("runs asynchronously", () => {
+  const input = [1, 2, 3, 4];
+
+  const output: number[] = [];
+  const cb = (x: number) => output.push(x);
+
+  return new Promise((resolve) => {
+    forEach(input, cb).then(() => {
+      expect(output).toEqual([0, ...input]);
+      resolve();
+    });
+    output.push(0);
+  });
+})
