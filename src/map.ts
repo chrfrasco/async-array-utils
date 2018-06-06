6a38e4b3684bc1433380fe0cfa8daa4f1d6500b6
Christian Scott <christianfscott@gmail.com>
2018-06-06 19:21:04 +1200

    improve map perf by pre-initialising array

--- src/map.ts
+++ src/map.ts
@@ -3,17 +3,17 @@ export default function map<T, S>(
   cb: (value: T, index?: number, array?: T[]) => S
 ): Promise<S[]> {
   return new Promise((resolve, reject) => {
-    const mapped: S[] = [];
+    const mapped: S[] = new Array(xs.length);
     let i = 0;
 
     function helper() {
-      if (mapped.length === xs.length) {
+      if (i === xs.length) {
         return resolve(mapped);
       }
 
       setImmediate(() => {
         try {
-          mapped.push(cb(xs[i], i, xs));
+          mapped[i] = cb(xs[i], i, xs);
           i += 1;
           helper();
         } catch (e) {
